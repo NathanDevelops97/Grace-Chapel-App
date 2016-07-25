af670fe626a2ffe002ecb19c6090b72fe79195b4
NathanDevelops97 <nathandevelops@gmail.com>
2016-07-25 09:57:49 -0400

    Started Xcode Project, Added Podfile, and Got Tab Bar Working
    Added code to app delegate for tab bar color

---
+++ App/Grace Chapel App/GiveViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GiveViewController.swift
+//  
+//
+//  Created by Nate on 7/25/16.
+//
+//
+
+import UIKit
+
+class GiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ App/Pods/Target Support Files/Pods-Grace Chapel App/Pods-Grace Chapel App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Grace_Chapel_App : NSObject
+@end
+@implementation PodsDummy_Pods_Grace_Chapel_App
+@end
