3a13116d2d3446e08811f876a8f4bda25d51c6e1
NathanDevelops97 <you@example.com>
2016-09-03 09:20:31 -0400

    Added new files to app. Starting fresh

--- App/Pods/Target Support Files/Pods-Grace Chapel App/Pods-Grace Chapel App-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Grace_Chapel_App : NSObject
-@end
-@implementation PodsDummy_Pods_Grace_Chapel_App
-@end
--- App/Pods/Target Support Files/Pods-Grace Chapel App/Pods-Grace Chapel App-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Grace_Chapel_AppVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Grace_Chapel_AppVersionString[];
-
