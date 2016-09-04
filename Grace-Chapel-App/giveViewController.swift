b4e35162df4b19dd7e44c3b59afff47e8d0aeaa3
NathanDevelops97 <you@example.com>
2016-09-03 22:56:25 -0400

    Added custom VC files and hooked them up to storyboard

---
+++ Grace-Chapel-App/giveViewController.swift
@@ -0,0 +1,35 @@
+//
+//  giveViewController.swift
+//  Grace-Chapel-App
+//
+//  Created by Nathanael Grant on 9/3/16.
+//  Copyright © 2016 Nathanael Grant. All rights reserved.
+//
+
+import UIKit
+
+class giveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
